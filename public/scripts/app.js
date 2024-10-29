ddd08ce2d7c617540db3f42a34dc3fd19cf5e27f
korodigergo <127103460+korodigergo@users.noreply.github.com>
2024-10-29 14:52:19 +0100

    lenis scroll and some designing

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -100,12 +100,11 @@ function createSelectElement() {
   ];
 
   for (let i = 0; i < categories.length; i++) {
-
     const option = document.createElement("option");
     option.value = categories[i];
     option.text = categories[i];
-    if(i === 0){
-      option.selected = true
+    if (i === 0) {
+      option.selected = true;
       option.disabled = true;
     }
     selectElement.appendChild(option);
@@ -118,6 +117,22 @@ function main() {
   loadProducts();
   createSelectElement();
   searchByCategory();
+
+  // Initialize Lenis
+  const lenis = new Lenis();
+
+  // Listen for the scroll event and log the event data
+  lenis.on("scroll", (e) => {
+
+  });
+
+  // Use requestAnimationFrame to continuously update the scroll
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
 }
 
 main();
