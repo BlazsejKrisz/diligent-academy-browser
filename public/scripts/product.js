8dec4507d5c20bee435b18856a9101902f8742b9
BlazsejKrisz <b.krisz4@gmail.com>
2024-10-31 13:25:52 +0100

    create product page with product info

---
+++ public/scripts/product.js
@@ -0,0 +1,25 @@
+const main = () => {
+    const productImg = document.createElement('img')
+    productImg.id = 'product-img'
+    productImg.src = product.img_url
+
+    const productHeader = document.createElement('h1')
+    productHeader.id = 'product-header'
+    productHeader.innerText = product.name
+
+    const productPrice = document.createElement('h3')
+    productPrice.id = 'product-price'
+    productPrice.innerText = `${product.price} $`
+
+    const productDescription = document.createElement('p')
+    productDescription.id = 'product-description'
+    productDescription.innerText = product.description
+
+    const productContainer = document.getElementsByClassName('product-container')
+    if (productContainer) {
+        productContainer.append(productImg, productHeader, productPrice, productDescription )
+    } else {
+        throw new Error('No product-countainer found')
+    }
+
+}
